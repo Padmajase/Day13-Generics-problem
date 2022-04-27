1941f012fdf229ee61e5379d2200ae50d5af3fbe
Padmajase <padmajapawar7@gmail.com>
2022-04-27 16:41:23 -0700

    Added : Generics program UC3 Refactor1

--- eclipse-workspace/Day13/src/main/java/com/bridgelabz/assignment/TestMaximum.java
+++ eclipse-workspace/Day13/src/main/java/com/bridgelabz/assignment/TestMaximum.java
@@ -12,40 +12,40 @@ import org.apache.log4j.Logger;
 public class TestMaximum {
     static Logger log = Logger.getLogger(TestMaximum.class);
 
-    // implementing method to get maximum of three integer numbers
-    public static int getThreeIntegerNumbers(Integer[] num) {
-        int max = 0;
-        for (int i=0; i<3; i++) {
-            if(max <= num[i]) {
-                max = num[i];
-            }
-        }
-        log.info("maximun number of given three integer numbers = " +max);
-        return max;
-    }
-
-    // implementing method to get maximum of three float numbers
-    public void getThreeFloatNumbers(Float[] floats) {
-        float max = 0;
-        for (int i=1; i<3; i++) {
-            if(max <= floats[i]) {
-                max = floats[i];
-            }
-        }
-        log.info("maximun number of given three float numbers = " + max);
-    }
-
+//    // implementing method to get maximum of three integer numbers
+//    public static int getThreeIntegerNumbers(Integer[] num) {
+//        int max = 0;
+//        for (int i=0; i<3; i++) {
+//            if(max <= num[i]) {
+//                max = num[i];
+//            }
+//        }
+//        log.info("maximun number of given three integer numbers = " +max);
+//        return max;
+//    }
+//
+//    // implementing method to get maximum of three float numbers
+//    public void getThreeFloatNumbers(Float[] floats) {
+//        float max = 0;
+//        for (int i=1; i<3; i++) {
+//            if(max <= floats[i]) {
+//                max = floats[i];
+//            }
+//        }
+//        log.info("maximun number of given three float numbers = " + max);
+//    }
 
     // implementing method to get string of maximum length
-    public void getThreeStrings(String[] string) {
+    public <E> void getAllItems(E[] item) {
         int l = 0;
-        int max = string[0].length();
-        for (int i=0; i<3; i++) {
-            if(max <= string[i].length()) {
-                max = string[i].length();
-                l = i;
-            }
-        }
+        E max = item[0];
+        max.compaeTo(item);
+//        for (int i=0; i<3; i++) {
+//            if(max <= item[i]) {
+//                max = item[i].length();
+//                l = i;
+//            }
+//        }
         log.info("maximun string of given three strings = " + string[l]);
     }
 
@@ -55,12 +55,11 @@ public class TestMaximum {
 
         TestMaximum generics = new TestMaximum();
         Integer[] a = {5, 3, 4 };
-        generics.getThreeIntegerNumbers(a);
-
         Float[] f = {1.2f, 1.9f, 1.3f};
-        generics.getThreeFloatNumbers(f);
-
         String[] s = {"Apple", "Peach", "Banana"};
-        generics.getThreeStrings(s);
+
+        generics.getAllItems(a);
+        generics.getAllItems(f);
+        generics.getAllItems(s);
     }
 }
