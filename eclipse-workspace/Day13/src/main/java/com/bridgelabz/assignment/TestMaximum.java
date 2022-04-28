c5141fb04dea7e6ef21735d549beecffe3c4a77b
Padmajase <padmajapawar7@gmail.com>
2022-04-27 22:42:04 -0700

    Added : Generics program UC4

--- eclipse-workspace/Day13/src/main/java/com/bridgelabz/assignment/TestMaximum.java
+++ eclipse-workspace/Day13/src/main/java/com/bridgelabz/assignment/TestMaximum.java
@@ -6,27 +6,24 @@ package com.bridgelabz.assignment;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
+import java.util.Scanner;
+
 /**
  * Adding class for finding maximum of three
  */
-class Generic<T extends Comparable <T>> {
+class Generic {
 
     static Logger log = Logger.getLogger(Generic.class);
 
-    // declaring parameter constructor
-    Generic(T[] a) {
-        log.info("parameterized constructor is called");
-    }
-
     // implementing method to get maximum of three integer numbers
-    public static int testMaximum(Integer[] num) {
+    public static <E> int testMaximum(int... num) {
         int max = 0;
-        for (int i=0; i<3; i++) {
-            if(max <= num[i]) {
-                max = num[i];
+        for ( int x : num) {
+            if(max <= x) {
+                max = x;
             }
         }
-        log.info("maximun number of given three integer numbers = " +max);
+        log.info("maximum number of given " + num.length+ " integer numbers = " +max);
         return max;
     }
 
@@ -34,8 +31,7 @@ class Generic<T extends Comparable <T>> {
         BasicConfigurator.configure();
         log.info("Welcome to generics !");
 
-        Integer[] a = {5, 3, 4 };
-        Generic generics = new Generic(a);
-        Generic.testMaximum(a);
+        Scanner sc = new Scanner(System.in);
+        Generic.testMaximum(2, 4, 55);
         }
 }
