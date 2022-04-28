686b299f6cb161d3bacf80675dc5b86339bcf776
Padmajase <padmajapawar7@gmail.com>
2022-04-27 17:17:05 -0700

    Added : Generics program UC3 Refactor2

--- eclipse-workspace/Day13/src/main/java/com/bridgelabz/assignment/TestMaximum.java
+++ eclipse-workspace/Day13/src/main/java/com/bridgelabz/assignment/TestMaximum.java
@@ -9,57 +9,33 @@ import org.apache.log4j.Logger;
 /**
  * Adding class for finding maximum of three
  */
-public class TestMaximum {
-    static Logger log = Logger.getLogger(TestMaximum.class);
+class Generic<T extends Comparable <T>> {
 
-//    // implementing method to get maximum of three integer numbers
-//    public static int getThreeIntegerNumbers(Integer[] num) {
-//        int max = 0;
-//        for (int i=0; i<3; i++) {
-//            if(max <= num[i]) {
-//                max = num[i];
-//            }
-//        }
-//        log.info("maximun number of given three integer numbers = " +max);
-//        return max;
-//    }
-//
-//    // implementing method to get maximum of three float numbers
-//    public void getThreeFloatNumbers(Float[] floats) {
-//        float max = 0;
-//        for (int i=1; i<3; i++) {
-//            if(max <= floats[i]) {
-//                max = floats[i];
-//            }
-//        }
-//        log.info("maximun number of given three float numbers = " + max);
-//    }
+    static Logger log = Logger.getLogger(Generic.class);
 
-    // implementing method to get string of maximum length
-    public <E> void getAllItems(E[] item) {
-        int l = 0;
-        E max = item[0];
-        max.compaeTo(item);
-//        for (int i=0; i<3; i++) {
-//            if(max <= item[i]) {
-//                max = item[i].length();
-//                l = i;
-//            }
-//        }
-        log.info("maximun string of given three strings = " + string[l]);
+    // declaring parameter constructor
+    Generic(T[] a) {
+        log.info("parameterized constructor is called");
+    }
+
+    // implementing method to get maximum of three integer numbers
+    public static int testMaximum(Integer[] num) {
+        int max = 0;
+        for (int i=0; i<3; i++) {
+            if(max <= num[i]) {
+                max = num[i];
+            }
+        }
+        log.info("maximun number of given three integer numbers = " +max);
+        return max;
     }
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
         log.info("Welcome to generics !");
 
-        TestMaximum generics = new TestMaximum();
         Integer[] a = {5, 3, 4 };
-        Float[] f = {1.2f, 1.9f, 1.3f};
-        String[] s = {"Apple", "Peach", "Banana"};
-
-        generics.getAllItems(a);
-        generics.getAllItems(f);
-        generics.getAllItems(s);
-    }
+        Generic generics = new Generic(a);
+        Generic.testMaximum(a);
+        }
 }
